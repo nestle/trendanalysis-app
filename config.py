5e66db1c2e8a6e22f6568ede5da966c2edee608d
ugog88 <42001474+ugog88@users.noreply.github.com>
2026-07-17 19:37:38 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,1 @@
-client_id = 'REDACTED_CLIENT_ID'
-client_secret = 'REDACTED_CLIENT_SECRET'
-deploymentName = 'ChatGPT'
-action = 'chat'
-action_extension = 'completions'
-api_version = '2023-07-01-preview'
 
-url = f'REDACTED_OPENAI_API_BASE_URL/deployments/{deploymentName}/{action}/{action_extension}?api-version={api_version}'
